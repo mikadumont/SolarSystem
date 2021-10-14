2a701adc1a74de9ed75847f8d01dc949ba23fbd3
Kendra Havens <kehavens@microsoft.com>
2021-10-14 15:16:48 -0700

    rss feed displaying

--- SolarSystem/Moon.cs
+++ SolarSystem/Moon.cs
@@ -6,6 +6,7 @@ namespace SolarSystem
     {
         public bool waning;
         public bool waxing;
+        public double daysTillFull;
         public static void LunarEclipse()
         {
 
@@ -14,6 +15,32 @@ namespace SolarSystem
         public static void CompletedOrbit() { }
         public static void CompletedRotation() { }
         public static void PullTides() { }
+
+        public static string FindDaysTillFullMoon()
+        {
+            DateTime today = DateTime.Now;
+
+            // Full Moon Phase Sept 2021
+            DateTime firstFullMoon = new DateTime(2021, 09, 20, 19, 55, 0);
+
+            // Full moons are *about* every 29.5 days
+            TimeSpan days = firstFullMoon.Subtract(today);
+            var findFullMoon = new DateTime();
+
+            while (today < findFullMoon)
+            {
+                findFullMoon.AddDays(29.5);
+            }
+
+            findFullMoon.AddDays(29.5);
+
+            var daysTill = today.Subtract(findFullMoon);
+
+            var moonsAgo = days / 29.5;
+
+            return daysTill.TotalDays.ToString();
+
+        }
 
     }
 
