a467520b1ce5b4698350b6af052e97f5c3f4a2c4
Kendra Havens <kehavens@microsoft.com>
2021-10-15 17:17:21 -0700

    add more tests and commented out table to razor

--- SolarSystem.Tests/VisiblePlanetsTests.cs
+++ SolarSystem.Tests/VisiblePlanetsTests.cs
@@ -1,8 +1,11 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+using System;
 using System.IO;
+using System.ServiceModel.Syndication;
 using System.Text.Json;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace SolarSystem.Tests
 {
@@ -12,16 +15,88 @@ namespace SolarSystem.Tests
         [TestMethod]
         public void PlanetDataParseTest()
         {
-            string fileName = "C:\\Code\\Demo\\2021 VSLaunch and NETConf\\BlazorServer SolarSystem2\\SolarSystem\\SolarSystem\\Data\\VisiblePlanetsData.json";
-            using FileStream openStream = File.OpenRead(fileName);
+            string filePath = Path.GetFullPath("Data\\VisiblePlanetsData.json");
+            using FileStream openStream = File.OpenRead(filePath);
+            DateTime today = DateTime.Today;
 
             VisiblePlanets[] planetDates = JsonSerializer.Deserialize<VisiblePlanets[]>(openStream);
-            foreach(VisiblePlanets planet in planetDates)
+
+            Console.WriteLine("Visible Planets:");
+
+            foreach (VisiblePlanets planet in planetDates)
             {
                 planet.ParseDateTime();
+                if (today.CompareTo(planet.start) > 0 && today.CompareTo(planet.end) < 0)
+                {
+                    Console.WriteLine(planet.Planet);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void RssFeedTest()
+        {
+            string url = "https://www.nasa.gov/rss/dyn/solar_system.rss";
+
+            XmlReader reader = XmlReader.Create(url);
+            SyndicationFeed feed = SyndicationFeed.Load(reader);
+            reader.Close();
+
+            Console.WriteLine("NASA RSS Feed");
+            string subject = "";
+            string summary;
+
+            foreach (SyndicationItem item in feed.Items)
+            {
+                subject = item.Title.Text;
+                summary = item.Summary.Text;
+
+                Console.WriteLine(subject);
+                Console.WriteLine(summary);
             }
 
-            int x = 0;
+            Assert.IsTrue(subject.Length > 1);
         }
+
+        [TestMethod]
+        public void PrintLongTruncation()
+        {
+            string url = "https://www.nasa.gov/rss/dyn/solar_system.rss";
+
+            XmlReader reader = XmlReader.Create(url);
+            SyndicationFeed feed = SyndicationFeed.Load(reader);
+            reader.Close();
+
+            Console.WriteLine("NASA RSS Feed:");
+            string subject = "";
+            string summary;
+
+            for (int i = 0; i < 100; i++)
+            {
+                foreach (SyndicationItem item in feed.Items)
+                {
+                    subject = item.Title.Text;
+                    summary = item.Summary.Text;
+
+                    Console.WriteLine(subject);
+                    Console.WriteLine(summary);
+                }
+            }
+
+            Assert.IsTrue(subject.Length > 1);
+        }
+
+        [TestMethod]
+        public void PrintStdErr()
+        {
+            Console.Error.WriteLine("This method throws IO File Not Found Exception." +
+                "\nStandard error writes also appear in Test Explorer output.");
+
+            // File Not Found Exception
+            string filePath = Path.GetFullPath("Non-existent-file.json");
+            using FileStream openStream = File.OpenRead(filePath);
+
+        }
+
     }
 }
